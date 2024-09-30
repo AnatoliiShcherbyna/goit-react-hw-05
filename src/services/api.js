ca780b7d64b83126c1d948045c19b68b8f1e3a3a
Anatolii Shcherbyna <anatoliiivshcherbyna@gmail.com>
2024-09-30 18:52:33 +0300

    Hw5

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_READ_ACCESS_TOKEN =
-  eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmNkMTY3ZDRhZDE3NDE1NzIyYWM4YjkxNWNiN2Q4YyIsIm5iZiI6MTcyNzQ1NjQ5Mi4zMTc4MTEsInN1YiI6IjY2ZjZlMWVhNmM5YTY4MTU1MDcwZTI3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KzppSI8RqeEnS6uGXu-Qnkxn7FTXNdSfXa-5jKQUoF0;
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDRlMmFiOWU4YmUxMTc5MDg1NTRiOWM2MTgyYmQxZSIsIm5iZiI6MTcyMzg5OTcwMC44MDYxNywic3ViIjoiNjZjMDlhMTQ3MWVjODliZDgzZDBhMzE5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9._kdrua01YV9kBIt_x9kJKzjIZlsDd31YGtF03otAZ3Y';
 
 const api = axios.create({
   baseURL: BASE_URL,
